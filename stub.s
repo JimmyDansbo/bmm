3f107e7a018787ee2ce325e64241ed08a11edfaf
Jimmy Dansbo <jimmy@dansbo.dk>
2026-01-23 14:53:54 +0100

    Updated jumptable to contain all current functions

--- stub.s
+++ stub.s
@@ -5,3 +5,10 @@ rem_space:	.word 0
 free_addr:	.word 0
 .segment "JUMPTABLE"
 	jmp	mm_init		; $A004
+	jmp	mm_set_isr	; $A007
+	jmp	mm_clear_isr	; $A00A
+	jmp	mm_alloc	; $A00D
+	jmp	mm_remaining	; $A010
+	jmp	mm_free		; $A013
+	jmp	mm_get_next_handle ; A016
+	jmp	mm_set_next_handle ; $A119
